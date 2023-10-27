645bfae7d77898f3cc6f01482e0c5daa1ced1f92
kathyhu91 <huchiachun@gmail.com>
2023-10-27 17:13:25 +0800

    main and yaoya proj init

---
+++ yaoya/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="container bg-warning p-3">
+    <button class="btn btn-primary">123456</button>
+    <button class="btn btn-success">123456</button>
+  </div>
+</template>
+<style lang="scss"></style>
---
+++ yaoya/src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <main>
+    <div class="btn">123</div>
+  </main>
+</template>
